4ea4a4ad45c1cfbe77f920cec69757e0bf8db6ca
Edson Andrez <edson.andrez@outlook.es>
2019-07-25 10:15:47 -0600

    Modelo proveedores

---
+++ app/Proveedor.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Proveedor extends Model
+{
+    //
+
+    protected $table = 'proveedores';
+    protected $primaryKey = 'id_proveedor';
+
+    protected  $fillable = [
+        'razon_social',
+        'nombre_comercial',
+        'nit',
+        'direccion_fiscal',
+        'direccion_planta',
+        'nombre_contacto',
+        'puesto_contacto',
+        'telefono_contacto',
+        'email',
+        'regimen_tributario',
+        'patente_comercio',
+        'patente_sociedad',
+        'dias_credito',
+        'monto_credito',
+        'horario_entrega',
+        'programa_bpm_implementado',
+        'otros_programas',
+        'sistema_haccp',
+        'programa_capacitacion',
+        'sistema_trazabilidad',
+        'sistema_calidad_auditado_intermamente',
+        'sistema_calidad_auditado_por_terceros',
+        'certificaciones'
+    ];
+
+    protected $dates = [
+        'created_at',
+        'updated_at'
+    ];
+}
