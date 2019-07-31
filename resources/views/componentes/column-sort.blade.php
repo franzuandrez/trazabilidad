e27b14edbb2f15a04cb66af3e57cdb29b32636eb
Edson Andrez <edson.andrez@outlook.es>
2019-07-31 12:11:15 -0600

    Componente para ordenamiento de columnas.

---
+++ resources/views/componentes/column-sort.blade.php
@@ -0,0 +1,13 @@
+
+<a href="javascript:ajaxLoad('{{url($modulo.'?field='.$field.'&sort='.($sort=='asc'?'desc':'asc'))}}')">
+    {{strtoupper($titulo)}}
+
+</a>
+
+@if($sortField==$field)
+    @if($sort=='asc')
+        <i class="fa fa-sort-asc" aria-hidden="true"></i>
+    @else
+        <i class="fa fa-sort-desc" aria-hidden="true"></i>
+    @endif
+@endif
