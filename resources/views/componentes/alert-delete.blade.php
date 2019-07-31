869509604d2197abdcb3e9ee9c187aabff1f695f
Edson Andrez <edson.andrez@outlook.es>
2019-07-31 15:59:19 -0600

    Componente para dar de baja algun modelo.

---
+++ resources/views/componentes/alert-delete.blade.php
@@ -0,0 +1,23 @@
+<div class="modal fade modal-slide-in-right" aria-hidden="true"
+     role="dialog" tabindex="-1" id="modal-delete-{{$id}}">
+    {{Form::Open(array('action'=>array($method,$id),'method'=>'delete'))}}
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title" align="center">ELIMINAR  {{$model}} : {{$description}} </h4>
+                <div class="modal-body" align="center">
+                    @if($extras !=null && $extras!='')
+                        {{$extras}}
+                    @endif
+                </div>
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal"><span class="fa fa-remove"></span> CERRAR</button>
+                <button type="submit" class="btn btn-default"><span class=" fa fa-check"></span> ACEPTAR</button>
+            </div>
+        </div>
+    </div>
+    {{Form::Close()}}
+
+</div>
