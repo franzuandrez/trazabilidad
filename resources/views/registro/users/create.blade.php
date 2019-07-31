8624f8919ceb9fe4e874515f41974dacdf385bfe
Edson Andrez <edson.andrez@outlook.es>
2019-07-31 16:55:03 -0600

    Se modifico la url del boton cancelar.

--- resources/views/registro/users/create.blade.php
+++ resources/views/registro/users/create.blade.php
@@ -71,7 +71,7 @@
         <div class="col-lg-12 col-sm-12 col-md-12 col-xs-12">
             <div class="form-group">
                 <button class="btn btn-default" type="submit"><span class=" fa fa-check"></span> GUARDAR</button>
-                <a href="{{url('users.index')}}"><button  class="btn btn-default" type="button"><span class="  fa fa-remove"></span> CANCELAR</button></a>
+                <a href="{{url('users')}}"><button  class="btn btn-default" type="button"><span class="  fa fa-remove"></span> CANCELAR</button></a>
 
             </div>
         </div>
