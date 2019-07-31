352043bd3d3a5dace56b7e000af50a3424c0db91
Edson Andrez <edson.andrez@outlook.es>
2019-07-31 16:05:48 -0600

    Alerta de no seleccion de rol.

--- resources/views/registro/roles/ajax.blade.php
+++ resources/views/registro/roles/ajax.blade.php
@@ -18,6 +18,11 @@
             @endslot
     @endcomponent
 
+    @component('componentes.alert-no-selecction')
+        @slot('mensaje')
+            SELECCIONE UN ROL
+        @endslot
+    @endcomponent
 
     <div class="row">
         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
