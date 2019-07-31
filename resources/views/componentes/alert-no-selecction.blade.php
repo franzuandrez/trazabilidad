bfc409e0aaa9b6b8518540620faa2a6e354d98ea
Edson Andrez <edson.andrez@outlook.es>
2019-07-31 16:05:04 -0600

    Componente - Alerta de no seleccion.

---
+++ resources/views/componentes/alert-no-selecction.blade.php
@@ -0,0 +1,16 @@
+<div class="modal fade modal-slide-in-right" aria-hidden="true"
+     role="dialog" tabindex="-1" id="errorToEdit">
+    {
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+
+                <h4 class="modal-title" align="center">{{$mensaje}}</h4>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal"><span class="fa fa-check"></span> ACEPTAR</button>
+            </div>
+        </div>
+    </div>
+
+</div>
