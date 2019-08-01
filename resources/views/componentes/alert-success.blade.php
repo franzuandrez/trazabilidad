91aa8c89c68563b4720ff48e20e6182242f98789
Edson Andrez <edson.andrez@outlook.es>
2019-08-01 11:22:59 -0600

    Componente - Alerta de exito.

---
+++ resources/views/componentes/alert-success.blade.php
@@ -0,0 +1,6 @@
+@if ($message = Session::get('success'))
+    <div class="alert alert-success alert-dismissible">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+        <p>{{ $message }}</p>
+    </div>
+@endif
