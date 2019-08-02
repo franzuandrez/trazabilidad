08e7177d2b7cdf11ca5248a9a89b280d6b22eb96
Edson Andrez <edson.andrez@outlook.es>
2019-08-02 16:42:46 -0600

    Parametro de url en el componente para eliminar algun modelo.

--- resources/views/componentes/alert-delete.blade.php
+++ resources/views/componentes/alert-delete.blade.php
@@ -14,7 +14,7 @@
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-default" data-dismiss="modal"><span class="fa fa-remove"></span> CERRAR</button>
-                <button type="button" onclick="javascipt:darBaja('{{url('users/')}}{{'/'.$id}}')" class="btn btn-default"><span class=" fa fa-check"></span> ACEPTAR</button>
+                <button type="button" onclick="javascipt:darBaja('{{$url}}')" class="btn btn-default"><span class=" fa fa-check"></span> ACEPTAR</button>
             </div>
         </div>
     </div>
