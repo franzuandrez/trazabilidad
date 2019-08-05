36ebf0dfc628c917a6a6c6ebaec6f226336e2c6b
Edson Andrez <edson.andrez@outlook.es>
2019-08-05 16:32:27 -0600

    Modelo de clientes,

---
+++ app/Cliente.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
+
+class Cliente extends Model
+{
+
+    use LogsActivity;
+
+    protected $table = 'clientes';
+    protected $primaryKey = 'id_categoria';
+
+    protected $fillable = [
+        'razon_social',
+        'nit',
+        'direccion',
+        'telefono',
+        'ruta',
+        'lunes',
+        'martes',
+        'miercoles',
+        'jueves',
+        'viernes',
+        'sabado',
+        'domingo',
+        'latitud',
+        'longitud',
+        'id_categoria'
+
+    ];
+
+    public static $logAttributes =[
+
+        'razon_social',
+        'nit',
+        'direccion',
+        'telefono',
+        'ruta',
+        'lunes',
+        'martes',
+        'miercoles',
+        'jueves',
+        'viernes',
+        'sabado',
+        'domingo',
+        'latitud',
+        'longitud',
+        'id_categoria'
+
+    ];
+
+    protected static $logOnlyDirty = true;
+
+    public function categoria(){
+
+        return $this->belongsTo('App\CategoriaCliente','id_categoria');
+    }
+
+
+}
