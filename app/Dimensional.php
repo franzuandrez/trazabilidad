a8006a58f061e10003b637bdd74bf6b7d315b937
Edson Andrez <edson.andrez@outlook.es>
2019-08-05 10:02:10 -0600

    Modelo de Dimensionales.

---
+++ app/Dimensional.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
+class Dimensional extends Model
+{
+    //
+    use LogsActivity;
+    protected $table = 'dimensionales';
+    protected $primaryKey = 'id_dimensional';
+
+    protected $fillable = [
+        'descripcion',
+        'unidad_medida',
+        'factor',
+        'estado'
+    ];
+
+    public static $logAttributes = [
+        'descripcion',
+        'unidad_medida',
+        'factor',
+        'estado'
+    ];
+
+    protected static $logOnlyDirty = true;
+
+    public function scopeActived($query){
+
+        return $query->where('dimensionales.estado',1);
+
+    }
+
+}
