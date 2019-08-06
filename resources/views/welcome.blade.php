8b4c809c73cc750a057e03bd1730340da271f076
Jairo Veláquez <jairo.j5180@gmail.com>
2019-08-06 12:11:01 -0600

    Primera prueba

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
+        {{--prueba de modificacion--}}
         <!-- Styles -->
         <style>
             html, body {
