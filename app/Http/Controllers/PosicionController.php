47a62a37db1d2c143f3c77f7afd026d09d8e7a28
Edson Andrez <edson.andrez@outlook.es>
2019-08-07 13:02:10 -0600

    Listar posiciones.

--- app/Http/Controllers/PosicionController.php
+++ app/Http/Controllers/PosicionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Posicion;
 use Illuminate\Http\Request;
 
 class PosicionController extends Controller
@@ -15,8 +16,31 @@ class PosicionController extends Controller
 
     public function index(Request $request){
 
+        $search = $request->get('search') == null ? '' : $request->get('search');
+        $sort = $request->get('sort') == null ? 'desc' : ($request->get('sort'));
+        $sortField = $request->get('field') == null ? 'codigo_barras' : $request->get('field');
+
+        $posiciones = Posicion::join('nivel','nivel.id_nivel','=','posiciones.id_nivel')
+            ->select('posiciones.*','nivel.descripcion as nivel')
+            ->actived()
+            ->where(function ( $query ) use ( $search) {
+                $query->where('posiciones.codigo_barras','LIKE','%'.$search.'%')
+                    ->orwhere('posiciones.descripcion','LIKE','%'.$search.'%')
+                    ->orwhere('nivel.descripcion','LIKE','%'.$search.'%');
+            })
+            ->orderBy($sortField,$sort)
+            ->paginate(20);
+
+        if($request->ajax()){
+            return view('registro.posiciones.index',
+                compact('posiciones','sort','sortField','search'));
+
+        }else{
+            return view('registro.posiciones.ajax',
+                compact('posiciones','sort','sortField','search'));
+
+        }
 
 
-        return view('registro.posiciones.ajax');
     }
 }
