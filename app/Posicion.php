4bab265c1b06cefc1bf9304f4dc1915fc09fffc3
Edson Andrez <edson.andrez@outlook.es>
2019-08-07 12:53:20 -0600

    Modelo de posiciones.

---
+++ app/Posicion.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
+
+class Posicion extends Model
+{
+    //
+    use LogsActivity;
+
+    protected $table = 'posiciones';
+    protected $primaryKey = 'id_posicion';
+    public $timestamps = false;
+
+    protected $fillable = [
+
+        'id_nivel',
+        'codigo_barras',
+        'descripcion',
+        'estado'
+    ];
+
+    public static $logAttributes = [
+        'id_nivel',
+        'codigo_barras',
+        'descripcion',
+        'estado'
+    ];
+
+    public static $logOnlyDirty = true;
+
+
+    public function scopeActived($query)
+    {
+        return $query->where('posiciones.estado', 1);
+
+    }
+
+    public function nivel()
+    {
+
+        return $this->belongsTo('App\Nivel', 'id_nivel');
+    }
+}
