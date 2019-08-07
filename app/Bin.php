148d3f5525ed5fe15b5f17f9da682afb2b0d61a4
Edson Andrez <edson.andrez@outlook.es>
2019-08-07 15:18:40 -0600

    Modelo Bin.

---
+++ app/Bin.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
+class Bin extends Model
+{
+    //
+    use LogsActivity;
+    protected $table = 'bines';
+    protected $primaryKey = 'id_bin';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'id_posicion',
+        'codigo_barras',
+        'descripcion',
+        'estado'
+    ];
+
+    public static $logAttributes = [
+        'id_posicion',
+        'codigo_barras',
+        'descripcion',
+        'estado'
+    ];
+
+    protected $logOnlyDirty = true;
+    public function scopeActived($query){
+
+        return $query->where('bines.estado',1);
+    }
+
+    public function posicion(){
+
+        return $this->belongsTo('App\Posicion','id_posicion');
+
+
+    }
+
+}
