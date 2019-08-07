071acc2f2534b9293119825e97963d0a8e813ff6
Edson Andrez <edson.andrez@outlook.es>
2019-08-07 15:24:41 -0600

    Listar bines.

--- app/Http/Controllers/BinController.php
+++ app/Http/Controllers/BinController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Bin;
 use Illuminate\Http\Request;
 
 class BinController extends Controller
@@ -16,8 +17,29 @@ class BinController extends Controller
 
     public function index(Request $request){
 
+        $search = $request->get('search') == null ? '' : $request->get('search');
+        $sort = $request->get('sort') == null ? 'desc' : ($request->get('sort'));
+        $sortField = $request->get('field') == null ? 'codigo_barras' : $request->get('field');
 
+        $bines  = Bin::join('posiciones','posiciones.id_posicion','=','bines.id_posicion')
+            ->select('bines.*','posiciones.descripcion as posicion')
+            ->actived()
+            ->where(function ( $query )  use  ( $search ){
+                $query->where('bines.codigo_barras','LIKE','%'.$search.'%')
+                    ->orwhere('bines.descripcion','LIKE','%'.$search.'%')
+                    ->orwhere('posiciones.descripcion','LIKE','%'.$search.'%');
+            })
+            ->orderBy($sortField,$sort)
+            ->paginate(20);
 
-        return view('registro.bines.ajax');
+        if($request->ajax()){
+            return view('registro.bines.index',
+                compact('bines','search','sort','sortField'));
+        }else{
+            return view('registro.bines.ajax',
+                compact('bines','search','sort','sortField'));
+
+        }
+
     }
 }
--- resources/views/registro/bines/ajax.blade.php
+++ resources/views/registro/bines/ajax.blade.php
@@ -37,6 +37,7 @@
         @endslot
     @endcomponent
     <div id="content">
+        @include('registro.bines.index')
     </div>
     <div class="loading">
         <i class="fa fa-refresh fa-spin "></i><br/>
