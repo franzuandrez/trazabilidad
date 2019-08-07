318237a603b0647625929f2f6e1d814bb69f37d8
Edson Andrez <edson.andrez@outlook.es>
2019-08-07 13:32:43 -0600

    Redireccionamiento corregido.

--- resources/views/registro/posiciones/create.blade.php
+++ resources/views/registro/posiciones/create.blade.php
@@ -99,7 +99,7 @@
             <button class="btn btn-default" type="submit">
                 <span class=" fa fa-check"></span> GUARDAR
             </button>
-            <a href="{{url('registro/niveles')}}">
+            <a href="{{url('registro/posiciones')}}">
                 <button class="btn btn-default" type="button">
                     <span class="fa fa-remove"></span>
                     CANCELAR
