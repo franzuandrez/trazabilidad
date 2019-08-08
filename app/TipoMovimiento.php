b8920d3ce35a716a9ca28683e3c63f09cb64c758
Edson Andrez <edson.andrez@outlook.es>
2019-08-08 11:09:40 -0600

    Modelo de Tipo Movimiento.

---
+++ app/TipoMovimiento.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
+
+class TipoMovimiento extends Model
+{
+    //
+    use LogsActivity;
+    protected $table = 'tipo_movimiento';
+    protected $primaryKey = 'id_movimiento';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'descripcion',
+        'factor',
+        'estado'
+    ];
+
+    public static $logAttributes = [
+        'descripcion',
+        'factor',
+        'estado'
+    ];
+
+    protected $logOnlyDirty = true;
+
+    public  function scopeActived( $query ){
+        return $query->where('estado',1);
+    }
+
+
+}
