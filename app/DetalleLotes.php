f2d2a7dacfe31d832fc27910cfe8cc69fe3dcc78
Edson Andrez <edson.andrez@outlook.es>
2019-08-08 13:26:17 -0600

    Modelo Detalla de lotes.

---
+++ app/DetalleLotes.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DetalleLotes extends Model
+{
+    //
+
+    protected  $table = 'detalle_lotes';
+    protected $primaryKey = 'id_detalle';
+    public $timestamps = false;
+
+
+    protected $fillable = [
+        'cantidad',
+        'no_lote',
+        'fecha_vencimiento',
+        'id_recepcion_enc'
+    ];
+
+    public function recepcion(){
+
+        return $this->belongsTo('App\Recepcion','id_recepcion_enc');
+    }
+
+}
