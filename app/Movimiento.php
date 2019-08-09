49ecf93b2bdb21461e6a59f21d0f1234a67feebc
Edson Andrez <edson.andrez@outlook.es>
2019-08-09 11:44:48 -0600

    Modelo de movimiento.

---
+++ app/Movimiento.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Movimiento extends Model
+{
+    //
+
+    protected $table = 'movimientos';
+    protected $primaryKey = 'id_movimiento';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'numero_documento',
+        'usuario',
+        'tipo_movimiento',
+        'cantidad',
+        'id_producto',
+        'fecha_hora_movimiento',
+        'ubicacion',
+        'lote',
+        'fecha_vencimiento',
+        'clave_autorizacion',
+        'estado'
+    ];
+
+    public function producto(){
+
+        return $this->belongsTo('App\Producto','id_producto');
+    }
+
+    public function usuario(){
+
+        return $this->belongsTo('App\User','usuario');
+    }
+}
