603131c60bad3020c67f554ac045bccde8d9328a
Edson Andrez <edson.andrez@outlook.es>
2019-08-09 12:10:04 -0600

    Js para recepcion.

--- resources/views/recepcion/materia_prima/ajax.blade.php
+++ resources/views/recepcion/materia_prima/ajax.blade.php
@@ -23,8 +23,6 @@
         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
             @component('componentes.btn-create',['url'=>url('recepcion/materia_prima/create')])
             @endcomponent
-            @component('componentes.btn-edit',['url'=>'javascript:editar()'])
-            @endcomponent
             @component('componentes.btn-ver',['url'=>'javascript:ver()'])
             @endcomponent
         </div>
@@ -45,4 +43,5 @@
 @endsection
 @section('scripts')
     <script src="{{asset('js/ajax-crud.js')}}"></script>
+    <script src="{{asset('js-brc/recepciones/index.js')}}"></script>
 @endsection
