b914a75ee63f724b7412525e6365493fc213a56d
Jairo Veláquez <jairo.j5180@gmail.com>
2019-08-13 08:52:09 -0600

    Rutas de peso Humedo

---
+++ resources/views/control/peso_humedo/create.blade.php
@@ -0,0 +1,340 @@
+@extends('layouts.admin')
+@section('style')
+    <link rel="stylesheet" href="{{asset('css/bootstrap-datepicker.css')}}">
+    <link rel="stylesheet" href="{{asset('css/bootstrap-timepicker.css')}}">
+@endsection
+
+@section('contenido')
+
+    @component('componentes.nav',['operation'=>'Ingreso',
+    'menu_icon'=>'fa fa-check-square-o',
+    'submenu_icon'=>'fa fa-th',
+    'operation_icon'=>'fa-plus',])
+        @slot('menu')
+            Control
+        @endslot
+        @slot('submenu')
+            Laminado
+        @endslot
+    @endcomponent
+
+
+    {!!Form::open(array('url'=>'control/laminado/create','method'=>'POST','autocomplete'=>'off'))!!}
+    {{Form::token()}}
+
+    <div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
+        <div class="form-group">
+            <label for="presentacion">RESPONSABLE</label>
+            <select name="id_presentacion" class="form-control selectpicker" id="presentacion">
+                <option value="">SELECCIONAR RESPONSABLE</option>
+                @foreach($responsables as $responsable)
+                    <option value="{{$responsable->id}}">{{$responsable->nombre}}</option>
+                @endforeach
+            </select>
+        </div>
+    </div>
+
+    <div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
+        <div class="form-group">
+            <label for="turno">TURNO</label>
+            <input id="turno" type="text"
+                   onkeydown="descomponerInput(this)"
+                   class="form-control">
+        </div>
+    </div>
+    <div class="col-lg-12 col-sm-12 col-md-12 col-xs-12">
+        <div class="">
+            <div class="tab-content">
+            </div>
+            <div class="tab-pane" id="tab_3">
+                <div class="col-lg-3 col-sm-6 col-md-6 col-xs-12">
+                    <label for="hora">HORA</label>
+                    <div class="input-group">
+                        <input id="hora" type="text" class="form-control timepicker" name="hora">
+
+                        <div class="input-group-addon">
+                            <i class="fa fa-clock-o"></i>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6 col-md-6 col-xs-12">
+                    <div class="form-group">
+                        <label for="temperatura">TEMPERATURA REPOSO 34-36 °C</label>
+                        <input id="temperatura" type="text" name="temperatura"
+
+                               class="form-control">
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6 col-md-6 col-xs-12">
+                    <div class="form-group">
+                        <label for="espesor">ESPESOR 1.25 A 1.30 (MILÍMETROS)</label>
+                        <input id="espesor" type="text" name="espesor"
+
+                               class="form-control">
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6 col-md-6 col-xs-12">
+                    <div class="form-group">
+                        <label for="lote_producto">LOTE PRODUCTO</label>
+                        <input id="lote_producto" type="text" name="lote_producto"
+
+                               class="form-control">
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6 col-md-6 col-xs-12">
+                    <div class="form-group">
+                        <label for="observaciones">OBSERVACIONES</label>
+                        <input id="observaciones" type="text" name="observaciones"
+
+                               class="form-control">
+                    </div>
+                </div>
+
+                <div class="col-lg-2 col-sm-4 col-md-2 col-xs-2">
+                    <br>
+                    <div class="form-group">
+                        <button id="btnAdd" class="btn btn-default block" style="margin-top: 5px;" type="button">
+                            <span class=" fa fa-plus"></span></button>
+                    </div>
+                </div>
+
+
+                <div class="col-lg-12 col-sm-12 col-md-12 col-xs-12">
+
+                    <table id="detalles" class="table table-striped table-bordered table-condensed table-hover">
+
+                        <thead style="background-color: #01579B;  color: #fff;">
+                        <th>OPCION</th>
+                        <th>HORA</th>
+                        <th>TEMPERATURA REPOSO 34-36 C</th>
+                        <th>ESPESOR 1.25 A 1.30 M</th>
+                        <th>LOTE PRODUCTO</th>
+                        <th>OBSERVACIONES</th>
+                        </thead>
+                        <tbody>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="col-lg-12 col-sm-12 col-md-12 col-xs-12">
+                    <div class="form-group">
+                        <label for="observacion_correctiva">OBSERVACIONES Y/O ACCION CORRECTIVA</label>
+                        <input type="text" name="observacion_correctiva" value="{{old('observacion_correctiva')}}"
+                               class="form-control">
+                    </div>
+                </div>
+                <div class="col-lg-12 col-sm-12 col-md-12 col-xs-12">
+                    <div class="form-group">
+                        <label for="observacion_correctiva">RESPONSABLE VERIFICADOR:</label>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
+                    <div class="form-group">
+                        <label for="puesto">PUESTO</label>
+                        <input type="text" name="puesto" value="{{old('puesto')}}"
+                               class="form-control">
+                    </div>
+                </div>
+                <div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
+                    <div class="form-group">
+                        <label for="nombre">NOMBRE</label>
+                        <input type="text" name="nombre" value="{{old('nombre')}}"
+                               class="form-control">
+                    </div>
+                </div>
+                <div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
+                    <div class="form-group">
+                        <label for="firma">FIRMA</label>
+                        <input type="text" name="firma" value="{{old('firma')}}"
+                               class="form-control">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+
+    <div class="col-lg-12 col-sm-12 col-md-12 col-xs-12">
+        <div class="form-group">
+            <button class="btn btn-default" type="submit">
+                <span class=" fa fa-check"></span> GUARDAR
+            </button>
+            <a href="{{url('control/mezcla_harina')}}">
+                <button class="btn btn-default" type="button">
+                    <span class="fa fa-remove"></span>
+                    CANCELAR
+                </button>
+            </a>
+
+        </div>
+    </div>
+    {!!Form::close()!!}
+
+@endsection
+@section('scripts')
+    <script>
+        $(function () {
+            //Timepicker
+            $('.timepicker').timepicker({
+                showInputs: false
+            });
+        })
+
+    </script>
+
+    <script>
+        $('.date').datepicker({
+            format: 'yyyy-mm-dd',
+            autoclose: true,
+            setDate: new Date()
+
+        });
+        $(document).ready(function () {
+
+            $("#btnAdd").click(function () {
+                addToTable();
+            });
+
+        });
+        $(window).keydown(function (event) {
+            if (event.keyCode == 13) {
+                event.preventDefault();
+                return false;
+            }
+        });
+    </script>
+    <script>
+        function cargarProveedores() {
+            //NOT IMPLEMENTED
+        }
+
+        function addToTable() {
+            if ($("#hora").val() != "" && $("#temperatura").val() != "" && $("#espesor").val() != "" && $("#lote_producto").val() != "" && $("#observaciones").val() != "") {
+                let hora = $("#hora");
+                let temperatura = $("#temperatura");
+                let espesor = $("#espesor");
+                let lote_producto = $("#lote_producto");
+                let observaciones = $("#observaciones");
+                let row =
+                    `<tr>
+            <td><button onclick=removeFromTable(this) type="button" class="btn btn-warning">x</button></td>
+            <td><input type="hidden" value='${hora.val()}' name=hora[]>${hora.val()}</td>
+            <td><input type="hidden" value='${temperatura.val()}' name=temperatura[]>${temperatura.val()}</td>
+            <td ><input type="hidden" value ='${espesor.val()}'  name=espesor[] >${espesor.val()}</td>
+            <td ><input type="hidden" value ='${lote_producto.val()}'  name=lote_producto[] >${lote_producto.val()}</td>
+            <td ><input type="hidden" value ='${observaciones.val()}'  name=observaciones[] >${observaciones.val()}</td>
+            </tr>`;
+
+                $("#detalles").append(row);
+                hora.val('');
+                temperatura.val('');
+                espesor.val('');
+                lote_producto.val('');
+                observaciones.val('');
+            } else {
+                $('#modal-default').modal('show');
+                return false;
+            }
+        }
+
+        function removeFromTable(element) {
+            //Removemos la fila
+            let td = $(element).parent();
+            td.parent().remove();
+            let tdNext = td.next();
+            let tdNextNext = tdNext.next();
+        }
+
+        function justNumbers(e) {
+            var keynum = window.event ? window.event.keyCode : e.which;
+            if ((keynum == 8) || (keynum == 46))
+                return true;
+
+            return /\d/.test(String.fromCharCode(keynum));
+        }
+
+        function limpiar() {
+
+            document.getElementById('id_producto').value = "";
+            document.getElementById('producto').value = "";
+            document.getElementById('proveedor').value = "";
+            document.getElementById('id_proveedor').value = "";
+            document.getElementById('producto').readOnly = false;
+            document.getElementById('buscar').disabled = false;
+        }
+
+        function mostrarProductosCargados() {
+
+            setTimeout(function () {
+                $('#modal-productos').modal();
+            }, 1000);
+        }
+
+        function mostrarAlertaNotFound() {
+            $('#not_found').modal();
+        }
+
+        function habilitar() {
+
+            document.getElementById('aceptar_producto').disabled = false;
+
+        }
+
+        function setProducto() {
+
+            let infoProd = getProductoSelected();
+            if (infoProd.length != 0) {
+                cargarProducto(infoProd);
+            } else {
+
+
+            }
+
+
+        }
+
+        function descomponerInput(input) {
+            const POSICION_CODIGO = 1;
+            const POSICION_FECHA = 2;
+            const POSICION_LOTE = 3;
+
+            var codigoBarras = input.value;
+            var removerParentesis = codigoBarras.replace(/\([0-9]*\)/g, '-');
+            var codigoSplited = removerParentesis.split('-');
+
+            var codigo = codigoSplited[POSICION_CODIGO];
+            var fecha = codigoSplited[POSICION_FECHA];
+            var lote = codigoSplited[POSICION_LOTE];
+
+            if (event.keyCode == 13) {
+                document.getElementById('lote').value = lote;
+                document.getElementById('producto').value = codigo;
+                document.getElementById('hora_carga').focus();
+            }
+        }
+
+        function getProductoSelected() {
+            var productos = document.getElementsByName('id_prod');
+            var id_prod = null;
+            var descripcion = null;
+            var id_prov = null;
+            var razon_social = null;
+
+            var arrayProductos = Object.keys(productos).map(function (key) {
+                return [Number(key), productos[key]];
+            });
+
+
+            arrayProductos.forEach(function (prod) {
+                if (prod[1].checked) {
+                    var childrens = prod[1].parentElement.parentElement.children;
+                    id_prod = childrens[0].firstChild.value;
+                    descripcion = childrens[2].innerText;
+                    razon_social = childrens[3].innerText;
+                    id_prov = childrens[3].firstChild.value;
+
+                }
+            });
+            return [id_prod, descripcion, id_prov, razon_social];
+        }
+    </script>
+@endsection
