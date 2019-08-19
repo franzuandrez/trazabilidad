907652e19ffdad8b325d37732fb2bb630a6ddbbb
Edson Andrez <edson.andrez@outlook.es>
2019-08-19 11:55:47 -0600

    redireccionar en caso de no accesso.

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -294,6 +294,12 @@
 <div class="box-body">
     <div class="row">
         <div class="col-md-12">
+            @if ($message = Session::get('unautorized'))
+                <div class="alert alert-danger alert-dismissible">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                    <p>{{ $message }}</p>
+                </div>
+            @endif
             @yield('contenido')
         </div>
     </div>
