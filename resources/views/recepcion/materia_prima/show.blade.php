a7171b7722ac123e27744bff78a799328e643c29
Edson Andrez <edson.andrez@outlook.es>
2019-08-21 09:54:59 -0600

    responsive table.

--- resources/views/recepcion/materia_prima/show.blade.php
+++ resources/views/recepcion/materia_prima/show.blade.php
@@ -521,7 +521,7 @@
                 <div class="tab-pane" id="tab_3">
 
 
-                    <div class="col-lg-12 col-sm-12 col-md-12 col-xs-12">
+                    <div class="col-lg-12 col-sm-12 col-md-12 col-xs-12 table-responsive">
 
                         <table id="detalles" class="table table-striped table-bordered table-condensed table-hover">
 
