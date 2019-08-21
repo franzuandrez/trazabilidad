f07094450096af1c6160d08965b2e06bd70c347e
Edson Andrez <edson.andrez@outlook.es>
2019-08-21 16:57:26 -0600

    modelo para el encabezado de produccion.

---
+++ app/Operacion.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Operacion extends Model
+{
+    //
+
+    protected $table = 'produccion_encabezado';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+
+    protected $fillable = [
+
+        'no_requision',
+        'no_orden_produccion',
+        'fecha_ingreso',
+        'id_usuario_ingreso',
+        'id_usuario_aprobo',
+
+    ];
+
+    public function usuario_ingreso() {
+
+        return $this->belongsTo('App\User','id_usuario_ingreso');
+    }
+
+    public function usuario_aprobo(){
+
+        return $this->belongsTo('App\User','id_usuario_aprobo');
+    }
+}
