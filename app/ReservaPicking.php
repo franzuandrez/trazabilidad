22601890567e1e3f5804bd1be3c9ab12b679e3bc
Edson Andrez <edson.andrez@outlook.es>
2019-08-27 11:15:07 -0600

    Relacion con bodega y producto de la reserva del picking.

--- app/ReservaPicking.php
+++ app/ReservaPicking.php
@@ -17,17 +17,28 @@ class ReservaPicking extends Model
         'lote',
         'cantidad',
         'id_requisicion',
+        'id_bodega'
     ];
 
-
+    public $with = [
+        'producto','bodega'
+    ];
     public function requisicion(){
 
 
         return $this->belongsTo('App\Requisicion','id_requisicion');
     }
 
+    public function producto(){
+        return $this->belongsTo('App\Producto','id_producto');
+    }
+
+    public function bodega(){
+        return $this->belongsTo('App\Bodega','id_bodega');
+
+    }
     public function scopeEnReserva( $query ){
-        return $query->where('reserva_lotes','R');
+        return $query->where('reserva_lotes.estado','R');
     }
 
 }
