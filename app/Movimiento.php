225a7285b9e8e004e0e02c6ce03b1d296415f815
Edson Andrez <edson.andrez@outlook.es>
2019-08-28 16:51:00 -0600

    Movimientos - Relaciones para obtener la ubicacion de un producto.

--- app/Movimiento.php
+++ app/Movimiento.php
@@ -26,31 +26,105 @@ class Movimiento extends Model
         'estado'
     ];
 
-    public function producto(){
+    /**
+     *           -----------------------RELATIONSHIPS ------------------------------------------
+     */
 
-        return $this->belongsTo('App\Producto','id_producto');
+    public function producto()
+    {
+
+        return $this->belongsTo('App\Producto', 'id_producto');
     }
 
 
-    public function usuario(){
+    public function usuario()
+    {
 
-        return $this->belongsTo('App\User','usuario');
+        return $this->belongsTo('App\User', 'usuario');
     }
 
-    public function scopeEnTransito( $query ){
+    public function orden_compra()
+    {
 
-        return $query->where('movimientos.ubicacion',0);
-    }
-
-    public function orden_compra(){
-
-        return $this->belongsTo('App\Recepcion','orden_compra','numero_documento');
+        return $this->belongsTo('App\Recepcion', 'orden_compra', 'numero_documento');
 
     }
 
-    public function bodega(){
+    public function localidad()
+    {
 
-        return $this->belongsTo('App\Bodega','ubicacion');
+        return $this->belongsTo('App\Localidad', 'id_localidad')->withDefault([
+            'descripcion' => 'Localidad por defecto'
+        ]);
+    }
+
+    public function bodega()
+    {
+
+        return $this->belongsTo('App\Bodega', 'id_bodega')->withDefault([
+            'descripcion' => 'Bodega por defecto'
+        ]);
 
     }
+
+    public function sector()
+    {
+        return $this->belongsTo('App\Sector', 'id_sector')->withDefault([
+            'descripcion' => 'Sector por defecto'
+        ]);
+    }
+
+    public function pasillo()
+    {
+
+        return $this->belongsTo('App\Pasillo', 'id_pasillo')->withDefault([
+            'descripcion' => 'Pasillo por Defecto'
+        ]);
+    }
+
+    public function rack()
+    {
+
+        return $this->belongsTo('App\Rack', 'id_rack')->withDefault([
+            'descripcion' => ' Rack por defecto',
+        ]);;
+
+    }
+
+    public function nivel()
+    {
+
+        return $this->belongsTo('App\Nivel', 'id_nivel')->withDefault([
+            'descripcion' => 'Nivel por defecto'
+        ]);
+
+    }
+
+    public function posicion()
+    {
+
+        return $this->belongsTo('App\Posicion', 'id_posicion')->withDefault([
+            'descripcion' => 'Posicion por defecto'
+        ]);
+    }
+
+    public function bin()
+    {
+        return $this->belongsTo('App\Bin', 'id_bin')->withDefault([
+            'descripcion' => 'Bin por defecto'
+        ]);
+    }
+
+    /**
+     * ---------------------------------SCOPES---------------------------------
+     *
+     */
+
+    public function scopeEnTransito($query)
+    {
+
+        return $query->where('movimientos.ubicacion', 0);
+    }
+
+
 }
