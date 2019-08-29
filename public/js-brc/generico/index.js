c586cf0ad11bf36dc509b4a863ad74c70f3d6ffa
Edson Andrez <edson.andrez@outlook.es>
2019-08-29 08:08:05 -0600

    Js generico - Js generico para el area de opciones de los diferentes catalogos y demas funcionalidades.

---
+++ public/js-brc/generico/index.js
@@ -0,0 +1,79 @@
+
+
+function editar(modulo){
+
+
+    let item = getItemSelected();
+
+
+    if(item ==null){
+        $('#errorToEdit').modal();
+    }else{
+        window.location.href = modulo+"/"+item+"/edit";
+    }
+
+}
+
+
+
+
+function getItemSelected(){
+
+    var items = document.getElementsByName('id_recepcion_enc');
+    var item=null;
+    var arrayItems = Object.keys(items).map(function(key) {
+        return [Number(key), items[key]];
+    });
+
+
+    arrayItems.forEach(function(element){
+        if(element[1].checked){
+            item = element[1].value;
+        }
+    });
+    return item;
+}
+
+
+function ver(modulo){
+    let item = getRecepcionSelected();
+
+    if(id_recepcion_enc==null){
+        $('#errorToEdit').modal();
+    }else{
+        window.location.href = modulo+"/"+item+"";
+    }
+
+}
+
+function darBaja(url){
+
+    var CSRF_TOKEN = $('meta[name="csrf-token"]').attr('content');
+
+    $.ajax({
+
+        url:url,
+        type:'post',
+        data:{_token:CSRF_TOKEN},
+        success:function(response){
+
+            $('.modal').modal('hide');
+
+            setTimeout(function () {
+                ajaxLoad(window.location.href);
+            },500);
+
+
+        },
+        error:function(e){
+
+
+
+        }
+
+
+    });
+
+
+
+}
