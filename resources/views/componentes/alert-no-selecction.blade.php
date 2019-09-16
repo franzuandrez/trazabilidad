5bba761073a807201cda956902330743ee98621c
Edson Andrez <edson.andrez@outlook.es>
2019-09-16 10:40:23 -0600

    id para el modal de no seleccion.

--- resources/views/componentes/alert-no-selecction.blade.php
+++ resources/views/componentes/alert-no-selecction.blade.php
@@ -5,7 +5,7 @@
         <div class="modal-content">
             <div class="modal-header">
 
-                <h4 class="modal-title" align="center">{{$mensaje}}</h4>
+                <h4 class="modal-title" id="no_selecction_mensaje" align="center">{{$mensaje}}</h4>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-default" data-dismiss="modal"><span class="fa fa-check"></span> ACEPTAR</button>
