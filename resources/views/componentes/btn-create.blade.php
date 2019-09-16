aad685d9f50ddccdcab77895ac914a3e3debc060
Edson Andrez <edson.andrez@outlook.es>
2019-09-16 11:21:09 -0600

    text decoration none para el boton de agregar.

--- resources/views/componentes/btn-create.blade.php
+++ resources/views/componentes/btn-create.blade.php
@@ -1,3 +1,4 @@
-<a href="{{$url}}" data-placement="top" title="Agregar" data-toggle="tooltip">
+<a style="text-decoration: none"
+    href="{{$url}}" data-placement="top" title="Agregar" data-toggle="tooltip">
     <img src="{{asset('imagenes_web/agregar.png')}}" width="50" height="50">
 </a>
