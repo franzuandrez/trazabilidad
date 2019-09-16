d058de0eca3b712cdf401af90ec84b0bd413d906
Edson Andrez <edson.andrez@outlook.es>
2019-09-16 11:49:21 -0600

    columna para saber que usuario esta haciendo el picking.

--- app/ReservaPicking.php
+++ app/ReservaPicking.php
@@ -17,28 +17,49 @@ class ReservaPicking extends Model
         'lote',
         'cantidad',
         'id_requisicion',
-        'id_bodega'
+        'id_bodega',
+        'id_ubicacion',
+        'ubicacion',
+        'id_usuario_picking'
     ];
 
     public $with = [
-        'producto','bodega'
+        'producto', 'bodega'
     ];
-    public function requisicion(){
+
+    public function requisicion()
+    {
 
 
-        return $this->belongsTo('App\Requisicion','id_requisicion');
+        return $this->belongsTo('App\Requisicion', 'id_requisicion');
     }
 
-    public function producto(){
-        return $this->belongsTo('App\Producto','id_producto');
+    public function producto()
+    {
+        return $this->belongsTo('App\Producto', 'id_producto');
     }
 
-    public function bodega(){
-        return $this->belongsTo('App\Bodega','id_bodega');
+    public function bodega()
+    {
+        return $this->belongsTo('App\Bodega', 'id_bodega');
 
     }
-    public function scopeEnReserva( $query ){
-        return $query->where('reserva_lotes.estado','R');
+
+    public function ubicacion()
+    {
+
+        return $this->belongsTo('App\Ubicacion', 'id_ubicacion');
+    }
+
+    public function scopeEnReserva($query)
+    {
+        return $query->where('reserva_lotes.estado', 'R');
+    }
+
+    public function usuario_picking()
+    {
+
+        return $this->belongsTo('App\User', 'id_usuario_picking');
     }
 
 }
