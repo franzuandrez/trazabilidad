233d9e627ae73b203095512f5aab69731e9d32e7
Edson Andrez <edson.andrez@outlook.es>
2019-11-27 14:37:54 -0600

    Se quita la validación del campo direccion fiscal a no requerida.

--- resources/views/registro/proveedores/create.blade.php
+++ resources/views/registro/proveedores/create.blade.php
@@ -66,7 +66,7 @@
         <div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
             <div class="form-group">
                 <label for="nombre">DIRECCION FISCAL</label>
-                <input type="text" name="direccion_fiscal" required value="{{old('direccion_fiscal')}}"
+                <input type="text" name="direccion_fiscal"  value="{{old('direccion_fiscal')}}"
                        class="form-control">
             </div>
         </div>
