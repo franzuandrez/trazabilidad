4cc6640839798d639ef241c397e28a00ec325113
Edson Andrez <edson.andrez@outlook.es>
2020-02-12 10:22:10 -0600

    opcion generica de en js de calidad.

--- public/js-brc/transito/index.js
+++ public/js-brc/transito/index.js
@@ -1,32 +1,28 @@
-
-
-function editar(){
+function editar() {
 
 
     let id_recepcion_enc = getRecepcionSelected();
 
 
-    if(id_recepcion_enc ==null){
+    if (id_recepcion_enc == null) {
         $('#errorToEdit').modal();
-    }else{
-        window.location.href = "transito"+"/"+id_recepcion_enc+"/ingreso";
+    } else {
+        window.location.href = "transito" + "/" + id_recepcion_enc + "/ingreso";
     }
 
 }
 
 
-
-
-function getRecepcionSelected(){
+function getRecepcionSelected() {
     var materia_prima = document.getElementsByName('id_recepcion_enc');
-    var id_recepcion_enc=null;
-    var arraymateria_prima = Object.keys(materia_prima).map(function(key) {
+    var id_recepcion_enc = null;
+    var arraymateria_prima = Object.keys(materia_prima).map(function (key) {
         return [Number(key), materia_prima[key]];
     });
 
 
-    arraymateria_prima.forEach(function(user){
-        if(user[1].checked){
+    arraymateria_prima.forEach(function (user) {
+        if (user[1].checked) {
             id_recepcion_enc = user[1].value;
         }
     });
@@ -34,15 +30,24 @@ function getRecepcionSelected(){
 }
 
 
-function ver(){
+function ver() {
     let id_recepcion_enc = getRecepcionSelected();
 
-    if(id_recepcion_enc==null){
+    if (id_recepcion_enc == null) {
         $('#errorToEdit').modal();
-    }else{
-        window.location.href = "transito"+"/"+id_recepcion_enc+"";
+    } else {
+        window.location.href = "transito" + "/" + id_recepcion_enc + "";
     }
 
 }
 
+function generico(url) {
+    let id_recepcion_enc = getRecepcionSelected();
 
+    if (id_recepcion_enc == null) {
+        $('#errorToEdit').modal();
+    } else {
+        window.location.href = url + "/" + id_recepcion_enc + "";
+    }
+
+}
