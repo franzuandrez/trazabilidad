c4ff0a71ab57194c7cca93d086ea73a4f5c7b6a3
Edson Andrez <edson.andrez@outlook.es>
2020-02-18 15:19:21 -0600

    Se agregó la tabla informativa del rango de los pesos.

--- resources/views/control/peso_humedo/create.blade.php
+++ resources/views/control/peso_humedo/create.blade.php
@@ -6,7 +6,14 @@
 
 @section('contenido')
     <div class="col-lg-12 col-lg-push-3col-sm-12   col-sm-push-3   col-md-12   col-md-push-3  col-xs-12">
-        <h3>CONTROL DE PESO HUMEDO DE PASTA PARA CHAO MEIN</h3>
+        <h3>CONTROL DE PESO HUMEDO DE PASTA PARA CHAO MEIN  <button
+                data-toggle="tooltip"
+                title="Informacion"
+                onclick="ver_informacion()"
+                type="button" class="btn btn-default btn-sm">
+                <i class="fa fa-info"
+                   aria-hidden="true"></i>
+            </button>  </h3>
     </div>
     @component('componentes.nav',['operation'=>'Ingreso',
     'menu_icon'=>'fa fa-check-square-o',
@@ -25,7 +32,7 @@
     {{Form::token()}}
 
     <input type="hidden" id="id_control" name="id_control">
-    <div class="col-lg-12 col-sm-12 col-md-12 col-xs-12">
+    <div class="col-lg-6 col-sm-12 col-md-12 col-xs-12">
         <label for="turno">NO ORDEN DE PRODUCCION</label>
         <div class="input-group">
             <input type="text" name="no_orden_produccion"
@@ -47,14 +54,7 @@
     </div>
 
 
-    <div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
-        <div class="form-group">
-            <label for="cortadora">CORTADORA NO.</label>
-            <input id="cortadora" type="text"
-                   disabled
-                   class="form-control">
-        </div>
-    </div>
+
 
     <div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
         <div class="form-group">
@@ -103,99 +103,100 @@
         </div>
     </div>
 
-
-    <div class="col-lg-3 col-sm-6 col-md-6 col-xs-12">
-        <div class="form-group">
-            <label for="no_1">NO. 1</label>
-            <input id="no_1" type="text" name="no_1"
-                   required
-                   disabled
-                   class="form-control">
-        </div>
-    </div>
-    <div class="col-lg-3 col-sm-6 col-md-6 col-xs-12">
-        <div class="form-group">
-            <label for="no_2">NO. 2</label>
-            <input id="no_2" type="text" name="no_2"
-                   required
-                   disabled
-                   class="form-control">
-        </div>
-    </div>
-    <div class="col-lg-3 col-sm-6 col-md-6 col-xs-12">
-        <div class="form-group">
-            <label for="no_3">NO. 3</label>
-            <input id="no_3" type="text" name="no_3"
-                   required
-                   disabled
-                   class="form-control">
-        </div>
-    </div>
-    <div class="col-lg-3 col-sm-6 col-md-6 col-xs-12">
-        <div class="form-group">
-            <label for="no_4">NO. 4</label>
-            <input id="no_4" type="text" name="no_4"
-                   required
-                   disabled
-                   class="form-control">
-        </div>
-    </div>
-
-    <div class="col-lg-3 col-sm-6 col-md-6 col-xs-12">
-        <div class="form-group">
-            <label for="no_5">NO. 5</label>
-            <input id="no_5" type="text" name="no_5"
-                   required
-                   disabled
-                   class="form-control">
-        </div>
-    </div>
-
-
-    <div class="col-lg-3 col-sm-6 col-md-6 col-xs-12">
-        <label for="observaciones">OBSERVACIONES</label>
-        <div class="input-group">
-            <input id="observaciones" type="text" name="observaciones"
-                   disabled
-                   class="form-control">
-            <div class="input-group-btn">
-                <button class="btn btn-default block"
-                        onclick="agregar_a_table()"
-                        style="margin-top: 5px;" type="button">
-                    <span class=" fa fa-plus"></span></button>
-                <button
-                    onclick="limpiar()"
-                    class="btn btn-default block" style="margin-top: 5px;" type="button">
-                    <span class=" fa fa-trash"></span></button>
+    <div class="col-lg-12 col-sm-12 col-md-12 col-xs-12">
+        <div class="col-lg-3 col-sm-6 col-md-6 col-xs-12">
+            <div class="form-group">
+                <label for="no_1">NO. 1</label>
+                <input id="no_1" type="text" name="no_1"
+                       required
+                       disabled
+                       class="form-control">
             </div>
         </div>
-    </div>
+        <div class="col-lg-3 col-sm-6 col-md-6 col-xs-12">
+            <div class="form-group">
+                <label for="no_2">NO. 2</label>
+                <input id="no_2" type="text" name="no_2"
+                       required
+                       disabled
+                       class="form-control">
+            </div>
+        </div>
+        <div class="col-lg-3 col-sm-6 col-md-6 col-xs-12">
+            <div class="form-group">
+                <label for="no_3">NO. 3</label>
+                <input id="no_3" type="text" name="no_3"
+                       required
+                       disabled
+                       class="form-control">
+            </div>
+        </div>
+        <div class="col-lg-3 col-sm-6 col-md-6 col-xs-12">
+            <div class="form-group">
+                <label for="no_4">NO. 4</label>
+                <input id="no_4" type="text" name="no_4"
+                       required
+                       disabled
+                       class="form-control">
+            </div>
+        </div>
 
-    <input type="hidden" name="hora" id="hora">
+        <div class="col-lg-3 col-sm-6 col-md-6 col-xs-12">
+            <div class="form-group">
+                <label for="no_5">NO. 5</label>
+                <input id="no_5" type="text" name="no_5"
+                       required
+                       disabled
+                       class="form-control">
+            </div>
+        </div>
 
 
+        <div class="col-lg-3 col-sm-6 col-md-6 col-xs-12">
+            <label for="observaciones">OBSERVACIONES</label>
+            <div class="input-group">
+                <input id="observaciones" type="text" name="observaciones"
+                       disabled
+                       class="form-control">
+                <div class="input-group-btn">
+                    <button class="btn btn-default block"
+                            onclick="agregar_a_table()"
+                           type="button">
+                        <span class=" fa fa-plus"></span></button>
+                    <button
+                        onclick="limpiar()"
+                        class="btn btn-default block" type="button">
+                        <span class=" fa fa-trash"></span></button>
+                </div>
+            </div>
+        </div>
 
-    <div class="col-lg-12 col-sm-12 col-md-12 col-xs-12 table-responsive">
+        <input type="hidden" name="hora" id="hora">
 
-        <table id="detalles" class="table table-striped table-bordered table-condensed table-hover">
 
-            <thead style="background-color: #01579B;  color: #fff;">
-            <tr>
-                <th>HORA</th>
-                <th>PRODUCTO</th>
-                <th>LOTE</th>
-                <th>NO. 1</th>
-                <th>NO. 2</th>
-                <th>NO. 3</th>
-                <th>NO. 4</th>
-                <th>NO. 5</th>
-                <th>OBSERVACIONES</th>
-            </tr>
+        <div class="col-lg-12 col-sm-12 col-md-12 col-xs-12 table-responsive">
 
-            </thead>
-            <tbody>
-            </tbody>
-        </table>
+            <table id="detalles" class="table table-striped table-bordered table-condensed table-hover">
+
+                <thead style="background-color: #01579B;  color: #fff;">
+                <tr>
+                    <th>HORA</th>
+                    <th>PRODUCTO</th>
+                    <th>LOTE</th>
+                    <th>NO. 1</th>
+                    <th>NO. 2</th>
+                    <th>NO. 3</th>
+                    <th>NO. 4</th>
+                    <th>NO. 5</th>
+                    <th>OBSERVACIONES</th>
+                </tr>
+
+                </thead>
+                <tbody>
+                </tbody>
+            </table>
+        </div>
+
     </div>
     <div class="col-lg-12 col-sm-12 col-md-12 col-xs-12">
         <div class="form-group">
@@ -205,7 +206,7 @@
         </div>
     </div>
 
-
+    @include('control.peso_humedo.tabla_informativa')
 
     <div class="col-lg-12 col-sm-12 col-md-12 col-xs-12">
         <div class="form-group">
@@ -450,6 +451,10 @@
             const fields = detalle();
             limpiar_formulario(fields)
 
+        }
+        function ver_informacion() {
+
+            $('#informacion').modal()
         }
 
 
