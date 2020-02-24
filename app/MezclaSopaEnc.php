2772253564059d4b7b9fa725efb987391e9eb2f8
Edson Andrez <edson.andrez@outlook.es>
2020-02-24 11:57:14 -0600

    Relacion del formulario de mezcla de sopas y la liberacion de sopas, y su detalle.

--- app/MezclaSopaEnc.php
+++ app/MezclaSopaEnc.php
@@ -11,4 +11,18 @@ class MezclaSopaEnc extends Model
     protected $table = 'mezclado_sopas_enc';
     protected $primaryKey = 'id_mezclado';
     public $timestamps = false;
+    protected $with = [
+        'detalle'
+    ];
+
+
+    public function control_trazabilidad()
+    {
+        return $this->belongsTo(Operacion::class, 'id_control', 'id_control');
+    }
+
+    public function detalle()
+    {
+        return $this->hasMany(MezclaSopaDet::class, 'id_mezclado_sopas_enc', 'id_mezclado');
+    }
 }
