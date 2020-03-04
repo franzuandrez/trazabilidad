e07ddfd8f97b2532b60c3d9201f10a5ac1febf92
Edson Andrez <edson.andrez@outlook.es>
2020-03-03 18:51:29 -0600

    Mostrar solo campos con algun valor.

--- resources/views/reportes/chaomein/linea_chaomein.blade.php
+++ resources/views/reportes/chaomein/linea_chaomein.blade.php
@@ -2,10 +2,11 @@
 <head>
     @include('reportes.parcials.pdf.head')
     <style>
-        .table-headers{
+        .table-headers {
             width: 90%;
         }
-        .table-headers{
+
+        .table-headers {
             margin-left: 0;
             margin-top: 0;
         }
@@ -17,7 +18,36 @@
 @include('reportes.parcials.pdf.header')
 
 <br>
-@include('reportes.parcials.pdf.detalle_header')
+@foreach( $reporte_detalle['headers']  as $key=> $header )
+    @if (!$loop->first)
+        <table class="table-headers">
+            <tr>
+                <th colspan="2" class="table-name">
+                    {{$key}}
+                </th>
+            </tr>
+            @foreach( $header as $h )
+                @if($h!=null)
+                    @if($h->value!="" && $h->value!=null)
+                        <tr>
+                            <th class="field">{{$h->field}}</th>
+                            @if($h->value=="0")
+                                <td>NO</td>
+                            @elseif($h->value == "1")
+                                <td>SI</td>
+                            @else
+                                <td>{{$h->value}}</td>
+                            @endif
+
+                        </tr>
+                    @endif
+                @endif
+            @endforeach
+        </table>
+        <br>
+    @endif
+@endforeach
+
 <br>
 
 
--- resources/views/reportes/sopas/linea_sopas.blade.php
+++ resources/views/reportes/sopas/linea_sopas.blade.php
@@ -17,7 +17,36 @@
 @include('reportes.parcials.pdf.header')
 
 <br>
-@include('reportes.parcials.pdf.detalle_header')
+@foreach( $reporte_detalle['headers']  as $key=> $header )
+    @if (!$loop->first)
+        <table class="table-headers">
+            <tr>
+                <th colspan="2" class="table-name">
+                    {{$key}}
+                </th>
+            </tr>
+            @foreach( $header as $h )
+                @if($h!=null)
+                    @if($h->value!="" && $h->value!=null)
+                        <tr>
+                            <th class="field">{{$h->field}}</th>
+                            @if($h->value=="0")
+                                <td>NO</td>
+                            @elseif($h->value == "1")
+                                <td>SI</td>
+                            @else
+                                <td>{{$h->value}}</td>
+                            @endif
+
+                        </tr>
+                    @endif
+                @endif
+            @endforeach
+        </table>
+        <br>
+    @endif
+@endforeach
+
 <br>
 
 
