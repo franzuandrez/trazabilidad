bc549216b744245b8a9bdce8e138a37ab3933104
Edson Andrez <edson.andrez@outlook.es>
2020-07-08 09:16:12 -0600

    Injeccion del PickingRepository dentro del picking Controller

--- app/Http/Controllers/PickingController.php
+++ app/Http/Controllers/PickingController.php
@@ -17,11 +17,11 @@ use Illuminate\Support\Facades\Auth;
 class PickingController extends Controller
 {
     //
-    protected $productos;
+    private $pickingRepository = null;
 
-    public function __construct(ExistenciasRepository $exitencias)
+    public function __construct(PickingRepository $pickingRepository)
     {
-        $this->productos = $exitencias;
+        $this->pickingRepository = $pickingRepository;
         $this->middleware('auth');
     }
 
@@ -121,8 +121,8 @@ class PickingController extends Controller
             $reserva = ReservaPicking::without('bodega')
                 ->findOrFail($id_reserva);
 
-
-            $debeRecalcular = $this->debeRecalcular($reserva->requisicion);
+            $this->pickingRepository->setOrdenRequisicion($reserva->requisicion);
+            $debeRecalcular = $this->pickingRepository->debeRecalcularseReserva();
             if ($debeRecalcular) {
                 $response = [
                     'status' => 2,
@@ -288,201 +288,5 @@ class PickingController extends Controller
 
     }
 
-    public function crearOrdenPicking($requisicion)
-    {
-
-        $existeOrden = Picking::where('id_requisicion', $requisicion->id)->exists();
-        if (!$existeOrden) {
-            $picking = new Picking();
-            $picking->id_requisicion = $requisicion->id;
-            $picking->fecha_inicio = Carbon::now();
-            $picking->estado = 'P';
-            $picking->save();
-        }
-    }
-
-    private function debeRecalcular($requisicion)
-    {
-
-
-        $ultimaReserva = $requisicion->reservas->last();
-        if ($ultimaReserva == null) {
-            $fechaUltimaReserva = Carbon::now()->format('Y-m-d H:i:s');
-        } else {
-            $fechaUltimaReserva = $ultimaReserva->created_at->format('Y-m-d H:i:s');
-        }
-
-        //DESPACHAS O EN RESERVAS QUE NO SEAN DE LA MISMA REQUISICION.
-        $reservas = ReservaPicking::where('id_requisicion', '<>', $requisicion->id)
-            ->where('fecha_lectura', '>', $fechaUltimaReserva)
-            ->get();
-
-        $productos = $reservas
-            ->pluck('id_producto')
-            ->toArray();
-
-        $lotes = $reservas
-            ->pluck('lote')
-            ->toArray();
-
-
-        $debeRecalcular = ReservaPicking::where('id_requisicion', $requisicion->id)
-            ->enProceso()
-            ->whereIn('id_producto', $productos)
-            ->whereIn('lote', $lotes)
-            ->exists();
-
-
-        return $debeRecalcular;
-    }
-
-
-    private function recalcular($requisicion)
-    {
-
-        try {
-            $this->borrarReservasNoLeidas($requisicion);
-            $this->generarListadoLotesDespachar($requisicion);
-
-
-        } catch (\Exception $ex) {
-
-            return $ex;
-            //DB::rollback();
-        }
-
-
-    }
-
-    private function borrarReservasNoLeidas($requisicion)
-    {
-        //DB::beginTransaction();
-
-
-        $ids_reservas = $requisicion
-            ->reservas()
-            ->enProceso()
-            ->pluck('id_reserva')
-            ->toArray();
-
-        //Borrar las reservas que no ha sido leidas.
-
-        DB::table('reserva_lotes')
-            ->whereIn('id_reserva', $ids_reservas)
-            ->delete();
-    }
-
-
-    private function reservas_previas($requisicion)
-    {
-
-        $reservas = $requisicion
-            ->reservas()
-            ->select('*', DB::raw('sum(cantidad) as total'))
-            ->groupBy('id_producto')
-            ->get();
-
-        return $reservas;
-
-    }
-
-    private function total_reservado($id_producto, $reservas)
-    {
-
-        $producto = $reservas
-            ->where('id_producto', $id_producto)
-            ->first();
-
-
-        //SI TIENE RESERVA, obtengo el total.
-        if ($producto != null) {
-            $cantidadReserva = $producto->total;
-        } else {
-            $cantidadReserva = 0;
-        }
-
-        return $cantidadReserva;
-
-    }
-
-    private function generarListadoLotesDespachar($requisicion)
-    {
-        //RESERVAS DE LA MISMA REQUISICION.
-        $reservas = $this->reservas_previas($requisicion);
-
-
-        $detalles_requisicion = $requisicion
-            ->detalle()
-            ->select('requisicion_detalle.id',
-                'requisicion_detalle.id_requisicion_encabezado',
-                'requisicion_detalle.orden_requisicion',
-                'requisicion_detalle.orden_produccion',
-                'requisicion_detalle.id_producto',
-                'requisicion_detalle.estado',
-                \DB::raw('sum(cantidad) as cantidad'))
-            ->groupBy('id_producto')
-            ->get();
-
-        foreach ($detalles_requisicion as $detalle_requisicion) {
-
-            //CANTIDAD YA RESERVADA. (0 en caso de no haber)
-            $cantidadReserva = $this->total_reservado($detalle_requisicion->id_producto, $reservas);
-
-            $cantidadEntrante = $detalle_requisicion->cantidad - $cantidadReserva;
-
-            // LOTES CON INVENTARIO DISPONIBLE
-            $lotes = $this
-                ->productos
-                ->existencia($detalle_requisicion->producto->codigo_barras)
-                ->map
-                ->only(['total', 'lote', 'fecha_vencimiento', 'ubicacion']);
-
-
-            //LOS LOTES QUE SI PODES UTILIZAR PORQUE NO HA SIDO RESERVADOS.
-            $lotesDisponibles = $this->getLotesDisponibles($lotes, $detalle_requisicion->id_producto);
-
-
-            if (!empty($lotesDisponibles)) {
-
-                foreach ($lotesDisponibles as $lote => $cantidadDisponible) {
-
-                    $codigo_ubicacion = explode('|', $lote)[1];
-                    $no_lote = explode('|', $lote)[0];
-
-                    $ubicacion = Sector::where('codigo_barras', $codigo_ubicacion)->first();
-
-                    $reserva = new ReservaPicking();
-                    $reserva->id_producto = $detalle_requisicion->id_producto;
-                    $reserva->lote = $no_lote;
-                    $reserva->fecha_vencimiento = $cantidadDisponible['fecha_vencimiento'];
-                    $reserva->id_requisicion = $detalle_requisicion->requision_encabezado->id;
-                    $reserva->id_bodega = $ubicacion->bodega->id_bodega;
-                    $reserva->ubicacion = $ubicacion->codigo_barras;
-                    $reserva->id_ubicacion = $ubicacion->id_sector;
-                    $reserva->estado = 'P';
-
-                    $esLoteConsumido = $cantidadEntrante >= $cantidadDisponible['total'];
-                    if ($esLoteConsumido) {
-                        $reserva->cantidad = $cantidadDisponible['total'];
-                        $reserva->save();
-                        $cantidadEntrante = $cantidadEntrante - $cantidadDisponible['total'];
-                    } else {
-                        $reserva->cantidad = $cantidadEntrante;
-                        if ($cantidadEntrante != 0) {
-                            $reserva->save();
-                        }
-                        $cantidadEntrante = 0;
-                    }
-
-
-                }
-            } else {
-                return redirect()->route('produccion.picking.index')
-                    ->withErrors(['No hay lotes disponibles']);
-            }
-        }
-
-
-    }
 
 }
