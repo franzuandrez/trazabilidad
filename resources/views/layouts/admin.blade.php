8746aafef47d16dd34c2fb190ca10c31de441060
Edson Andrez <edson.andrez@outlook.es>
2020-10-01 13:31:21 -0600

    Funcion para mostrar el orden en que deberia posicionarse el foco al momento de darle enter.

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -623,7 +623,14 @@
         }
 
     });
-
+    $('form').on('keydown', 'input', function (event) {
+        if (event.which == 13) {
+            event.preventDefault();
+            var $this = $(event.target);
+            var index = parseFloat($this.attr('data-index'));
+            $('[data-index="' + (index + 1).toString() + '"]').focus();
+        }
+    });
 </script>
 @yield('scripts')
 </body>
