f8f745aa23fdc97ed3a07a57b047646d2d769da1
Edson Andrez <edson.andrez@outlook.es>
2020-10-01 13:31:50 -0600

    Footer con el listado de firmas que deberia llevar el reporte en caso de existir.

--- resources/views/reportes/parcials/pdf/footer.blade.php
+++ resources/views/reportes/parcials/pdf/footer.blade.php
@@ -1,1 +1,26 @@
-<p> Generado en PDF </p>
+<br>
+<br>
+<br>
+@if(count($reporte_encabezado->getFirmas())>0 )
+    <table style=" border: none;">
+        @foreach($reporte_encabezado->getFirmas() as $key=>$value)
+            <tr style=" border: none;">
+                <td style=" border: none;"><b>{{$key}}</b></td>
+                <td style=" border: none;">F_______________________</td>
+            </tr>
+            <tr style=" border: none;">
+                <td style=" border: none;"></td>
+                <td style=" border: none;">{{$value}}</td>
+            </tr>
+            <tr>
+                <td style=" border: none;">&nbsp;</td>
+                <td style=" border: none;">&nbsp;</td>
+            </tr>
+            <tr>
+                <td style=" border: none;">&nbsp;</td>
+                <td style=" border: none;">&nbsp;</td>
+            </tr>
+        @endforeach
+
+    </table>
+@endif
