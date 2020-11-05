fd09dff96751eb2eb4a9d6dffa9515434a0d0afc
Edson Andrez <edson.andrez@outlook.es>
2020-11-05 13:54:52 -0600

    Reporte de trazabilidad hacia adelante

---
+++ resources/views/operaciones/hacia_adelante/index.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.admin')
+@section('style')
+    <link href="{{asset('css/loading.css')}}" rel="stylesheet">
+@endsection
+@section('contenido')
+    @include('componentes.alert-success')
+    @include('componentes.alert-error')
+
+    @component('componentes.nav',['operation'=>'LIST',
+    'menu_icon'=>'fa-file-text',
+    'submenu_icon'=>'fa fa-hand-lizard-o',
+    'operation_icon'=>'',])
+        @slot('menu')
+            Operaciones
+        @endslot
+        @slot('submenu')
+            Consulta trazabilidad
+        @endslot
+    @endcomponent
+
+    <div id="content">
+        @include('operaciones.hacia_adelante.ajax')
+    </div>
+
+@endsection
+
+@section('scripts')
+    <script src="{{asset('js/ajax-crud.js')}}"></script>
+@endsection
---
+++ resources/views/operaciones/trazabilidad/ajax_aux.blade.php
@@ -0,0 +1,7 @@
+
+    @if($producto_trazabilidad!=null)
+        @include('operaciones.trazabilidad.producto_trazabilidad')
+    @endif
+
+@include('operaciones.trazabilidad.eventos')
+
