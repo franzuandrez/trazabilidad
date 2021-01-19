236fe515063c59b4b99f0f58b4e1012193a85f4f
edson.andrez@outlook.es <7y3T^6zx2S$Yv^ve!GUm>
2021-01-19 09:14:07 -0600

    Manejo de la orden de produccion y de las existencias - Tools

---
+++ app/Http/tools/Existencias.php
@@ -0,0 +1,55 @@
+<?php
+
+
+namespace App\Http\tools;
+
+
+use App\Movimiento;
+use App\Producto;
+use DB;
+
+class Existencias
+{
+    /**
+     * @param $codigo_producto
+     * @return mixed
+     *  Devuelve la existencia del producto solicitado ordenado
+     * por el lote proximo a vencer, agrupado por bodegas.
+     */
+
+    public function existencia($codigo_producto)
+    {
+
+
+        $productos = Producto::where('codigo_interno', '=', $codigo_producto)
+            ->orWhere('codigo_barras', '=', $codigo_producto)
+            ->pluck('id_producto');
+
+
+        $existencias = Movimiento::join('tipo_movimiento', 'tipo_movimiento.id_movimiento', '=', 'movimientos.tipo_movimiento')
+            ->select('movimientos.id_movimiento',
+                'movimientos.lote',
+                'movimientos.id_producto',
+                'movimientos.id_bodega',
+                'movimientos.fecha_vencimiento',
+                'movimientos.ubicacion',
+                DB::raw('sum(cantidad * factor) as total'))
+            ->whereIn('id_producto', $productos)
+            ->where('movimientos.observaciones','=','')
+            ->groupBy('id_producto')
+            ->groupBy('lote')
+            ->groupBy('ubicacion')
+            ->orderBy('movimientos.fecha_vencimiento', 'asc')
+            ->with('producto')
+            ->with('bodega')
+            ->with('producto.presentacion')
+            ->with('producto.dimensional')
+            ->get();
+
+
+        $response = $existencias;
+        return $response;
+
+    }
+
+}
---
+++ app/Http/tools/OrdenProduccion.php
@@ -0,0 +1,132 @@
+<?php
+
+
+namespace App\Http\tools;
+
+
+use App\Correlativo;
+use App\LineaChaomin;
+use App\LineaSopa;
+use App\Requisicion;
+use Carbon\Carbon;
+use DB;
+
+class OrdenProduccion
+{
+
+
+    public static function obtener_nueva_no_orden()
+    {
+        $no_orden_produccion = Correlativo::where('modulo', 'PRODUCCION')
+            ->first();
+
+        $posible_no_orden_produccion = $no_orden_produccion->prefijo . '-' . Carbon::now()->addDay()
+                ->format('Ymd');
+
+        $cantidad = Requisicion::where('no_orden_produccion', 'LIKE', '%' . $posible_no_orden_produccion . '%')->count();
+
+
+        $no_orden_produccion = $no_orden_produccion->prefijo . '-' . Carbon::now()->addDay()
+                ->format('Ymd');
+
+        if ($cantidad > 0) {
+            $no_orden_produccion = $no_orden_produccion . '-' . $cantidad;
+        }
+
+        return $no_orden_produccion;
+
+
+    }
+
+
+    public static function verificar_linea_chaomin($no_orden_produccion)
+    {
+
+
+        $controles = DB::table('control_trazabilidad_orden_produccion')
+            ->where('no_orden_produccion', $no_orden_produccion)
+            ->get();
+
+        $linea_chamoin = LineaChaomin::with('control_trazabilidad.producto')
+            ->without('control_trazabilidad.requisiciones')
+            ->whereIn('id_control', $controles->pluck('id_control')->toArray())
+            ->get();
+
+        if (count($linea_chamoin) > 0) {
+            $response = [
+                'status' => 1,
+                'message' => 'Linea chaomin iniciada',
+                'data' => $linea_chamoin
+            ];
+        } else {
+            $response = [
+                'status' => 0,
+                'message' => 'Linea no existente',
+                'data' => ''
+            ];
+        }
+
+        return $response;
+
+
+    }
+
+
+    public static function verificar_linea_sopas($no_orden_produccion)
+    {
+        $controles = DB::table('control_trazabilidad_orden_produccion')
+            ->where('no_orden_produccion', $no_orden_produccion)
+            ->get();
+
+        $linea_sopa = LineaSopa::with('control_trazabilidad.producto')
+            ->whereIn('id_control', $controles->pluck('id_control')->toArray())
+            ->get();
+
+        if (count($linea_sopa) > 0) {
+            $response = [
+                'status' => 1,
+                'message' => 'Linea chaomin iniciada',
+                'data' => $linea_sopa
+            ];
+        } else {
+            $response = [
+                'status' => 0,
+                'message' => 'Linea no existente',
+                'data' => ''
+            ];
+        }
+
+        return $response;
+    }
+
+    public static function verificar_linea_chaomin_por_producto($no_orden_produccion, $id_producto)
+    {
+        $control = DB::table('control_trazabilidad_orden_produccion')
+            ->where('no_orden_produccion', $no_orden_produccion)
+            ->where('id_producto', $id_producto)
+            ->first();
+
+        $linea_chamoin = LineaChaomin::with('control_trazabilidad.producto')
+            ->without('control_trazabilidad.requisiciones')
+            ->where('id_control', $control->id_control)
+            ->get();
+
+        if ($linea_chamoin != null) {
+            $response = [
+                'status' => 1,
+                'message' => 'Linea chaomin iniciada',
+                'data' => $linea_chamoin
+            ];
+
+        } else {
+            $response = [
+                'status' => 0,
+                'message' => 'Linea chaomin iniciada',
+                'data' => $linea_chamoin
+            ];
+        }
+
+        return $response;
+
+    }
+}
